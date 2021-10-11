9278a12500468da28fee3bf875f91ba632bc8d53
Samuel Jonathan <Omohsam81@gmail.com>
2021-10-11 02:37:09 +0100

    Updated file

--- src/maphosa/Hello.java
+++ src/maphosa/Hello.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Hello {
 
     public static void main(String[] args) {
+        System.out.println("Program to test github setup");
         Scanner name = new Scanner(System.in);
         System.out.printf("Enter your name: ");
         String userName = name.nextLine();
